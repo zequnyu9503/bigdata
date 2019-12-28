475fdf1d588f468a8e545185d03cc97827f2e759
zequnyu9503 <greatyuzequn@outlook.com>
2019-12-28 10:30:37 +0800

    fix usingIterator

--- run/run-timewindow.workload.usingiterator.sh
+++ run/run-timewindow.workload.usingiterator.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-scala -cp pers.yzq.timewindow.prefetcher.loader.UsingIterator ../timewindow/target/timewindow-init.jar
+scala -classpath pers.yzq.timewindow.prefetcher.loader.UsingIterator ../timewindow/target/timewindow-init.jar \
+-jar  /home/zc/yzq/libs/target/dependency/hadoop-common-3.1.0.jar, \
+/home/zc/yzq/libs/target/dependency/hadoop-mapreduce-client-core-2.7.7.jar
