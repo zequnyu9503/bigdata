576415fc46198ffbf56f5e3cbf5718c07b52ce12
zequnyu9503 <greatyuzequn@outlook.com>
2019-12-28 13:45:28 +0800

    fix usingIterator

--- timewindow/src/main/scala/pers/yzq/timewindow/prefetcher/loader/UsingIterator.scala
+++ timewindow/src/main/scala/pers/yzq/timewindow/prefetcher/loader/UsingIterator.scala
@@ -93,6 +93,7 @@ object UsingIterator {
 
   def getJobConf: JobConf = {
     val conf = new Configuration()
+    conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem")
     val newJobConf = new JobConf(conf)
     FileInputFormat.setInputPaths(newJobConf, new Path(input))
     newJobConf
