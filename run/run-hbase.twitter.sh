a1a568d6c8adbe4fb0276f9cce6b20547a3bba07
zequnyu9503 <greatyuzequn@outlook.com>
2020-06-01 22:01:30 +0800

    Fix bugs.

--- run/run-hbase.twitter.sh
+++ run/run-hbase.twitter.sh
@@ -22,6 +22,7 @@ ${libs_dir}/htrace-core-3.1.0-incubating.jar,\
 ${libs_dir}/htrace-core4-4.2.0-incubating.jar,\
 ${libs_dir}/hbase-hadoop-compat-2.1.4.jar,\
 ${libs_dir}/hbase-hadoop2-compat-2.1.4.jar,\
+${libs_dir}/hbase-metrics-2.1.4.jar,\
 ${libs_dir}/hbase-server-2.1.4.jar,\
 ${libs_dir}/fastjson-1.2.35.jar \
 hbase/target/hbase-init.jar
